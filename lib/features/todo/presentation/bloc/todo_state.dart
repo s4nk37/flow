13b544358040cba5471d7f71c0733a604154c736
Sanket Patel <44511437+s4nk37@users.noreply.github.com>
2024-02-23 01:33:26 +0530

    Added Homescreen

--- lib/features/todo/presentation/bloc/todo_state.dart
+++ lib/features/todo/presentation/bloc/todo_state.dart
@@ -1,9 +1,10 @@
 part of 'todo_bloc.dart';
 
-abstract class TodoState extends Equatable {
-  const TodoState();  
-
+sealed class TodoState extends Equatable {
+  const TodoState();
+  
   @override
   List<Object> get props => [];
 }
-class TodoInitial extends TodoState {}
+
+final class TodoInitial extends TodoState {}
