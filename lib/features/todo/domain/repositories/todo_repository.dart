6da4495653b073367f64ddd6fd9b8b8a6bcf5ad4
Sanket Patel <44511437+s4nk37@users.noreply.github.com>
2024-02-26 14:35:12 +0530

    🎨 Add NoParams Replacing Void

--- lib/features/todo/domain/repositories/todo_repository.dart
+++ lib/features/todo/domain/repositories/todo_repository.dart
@@ -1,16 +1,17 @@
 import 'package:dartz/dartz.dart';
 
 import '../../../../core/errors/failures.dart';
+import '../../../../core/usecases/usecase.dart';
 import '../entities/todo.dart';
 
 abstract class TodoRepository {
   Future<Either<Failure, List<Todo>>> getTodos();
   Future<Either<Failure, Todo>> getTodoById(int id);
-  Future<Either<Failure, void>> addTodo(Todo todo);
-  Future<Either<Failure, void>> updateTodo(Todo todo);
-  Future<Either<Failure, void>> deleteTodoById(int id);
-  Future<Either<Failure, void>> deleteAllTodos();
-  Future<Either<Failure, void>> deleteCompletedTodos();
-  Future<Either<Failure, void>> markTodoAsCompleted(int id);
-  Future<Either<Failure, void>> markTodoAsIncompleted(int id);
+  Future<Either<Failure, NoParams>> addTodo(Todo todo);
+  Future<Either<Failure, NoParams>> updateTodo(Todo todo);
+  Future<Either<Failure, NoParams>> deleteTodoById(int id);
+  Future<Either<Failure, NoParams>> deleteAllTodos();
+  Future<Either<Failure, NoParams>> deleteCompletedTodos();
+  Future<Either<Failure, NoParams>> markTodoAsCompleted(int id);
+  Future<Either<Failure, NoParams>> markTodoAsIncompleted(int id);
 }
