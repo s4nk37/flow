5a6a7385133a8b4a94eb5b4b4c5a797f8a1ecb9c
Sanket Patel <44511437+s4nk37@users.noreply.github.com>
2024-02-26 19:15:44 +0530

    Add: from JSON test

--- test/features/todo/data/models/todo_model_test.dart
+++ test/features/todo/data/models/todo_model_test.dart
@@ -1,14 +1,18 @@
+import 'dart:convert';
+
 import 'package:flow/features/todo/data/models/todo_model.dart';
 import 'package:flow/features/todo/domain/entities/todo.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import '../../../../fixtures/fixture_reader.dart';
+
 void main() {
   final tTodoModel = TodoModel(
-    id: 1,
-    title: 'Test',
-    description: 'Test',
+    id: 2,
+    title: 'Prepare for meeting',
+    description: 'Gather presentation slides and notes',
     isCompleted: false,
-    createdAt: DateTime.now(),
+    createdAt: DateTime(2017,9,7),
   );
 
   test('Should be a subclass of Todo entity', () async {
@@ -18,5 +22,16 @@ void main() {
 
     // assert
     expect(tTodoModel, isA<Todo>());
+  });
+
+  group('fromJson', () {
+    test('Should return a valid TodoModel when JSON ID is integer', () {
+      // arrange
+      final Map<String, dynamic> jsonMap = jsonDecode(fixture('todo.json'));
+      // act
+      final result = TodoModel.fromJson(jsonMap);
+      // assert
+      expect(result, equals(tTodoModel));
+    });
   });
 }
