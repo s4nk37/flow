b6cf61ae4dff6c8f6d2b0156dd0c61718f9f10d7
Sanket Patel <44511437+s4nk37@users.noreply.github.com>
2024-02-27 19:39:21 +0530

    Add: Todo Operations

--- lib/features/todo/data/datasources/todo_local_datasource.dart
+++ lib/features/todo/data/datasources/todo_local_datasource.dart
@@ -2,9 +2,17 @@ import '../../domain/entities/todo.dart';
 
 abstract class TodoLocalDataSource {
   /// Gets the cached [List<Todo>] which was gotten the last time
-  /// the user had an internet connection  
-  /// 
+  /// the user had an internet connection
+  ///
   /// Throws [CacheException] if no cached data is present
   Future<List<Todo>> getTodos();
   Future<void> cacheTodos(List<Todo> todos);
+  Future<Todo> getTodoById(int id);
+  Future<void> addTodo(Todo todo);
+  Future<void> updateTodo(Todo todo);
+  Future<void> deleteTodoById(int id);
+  Future<void> deleteAllTodos();
+  Future<void> deleteCompletedTodos();
+  Future<void> markTodoAsCompleted(int id);
+  Future<void> markTodoAsIncompleted(int id);
 }
