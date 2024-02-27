36f8e85db38fda938f2142d8817bd5d4a58aeab1
Sanket Patel <44511437+s4nk37@users.noreply.github.com>
2024-02-27 20:02:24 +0530

    🎨 Improved Code

--- lib/features/todo/data/repositories/todo_repository_impl.dart
+++ lib/features/todo/data/repositories/todo_repository_impl.dart
@@ -13,62 +13,27 @@ class TodoRepositoryImpl implements TodoRepository {
   final TodoRemoteDataSource remoteDataSource;
   final NetworkInfo networkInfo;
 
-  TodoRepositoryImpl(
-      {required this.localDataSource,
-      required this.remoteDataSource,
-      required this.networkInfo});
+  TodoRepositoryImpl({
+    required this.localDataSource,
+    required this.remoteDataSource,
+    required this.networkInfo,
+  });
 
-  @override
-  Future<Either<Failure, NoParams>> addTodo(Todo todo) async{
-   if (await networkInfo.isConnected) {
-      try {
-        await remoteDataSource.addTodo(todo);
-        return Right(NoParams());
-      } on ServerException {
-        return Left(ServerFailure());
-      }
-    } else {
-      try {
-        await localDataSource.addTodo(todo);
-        return Right(NoParams())  ;
-      } on CacheException {
-        return Left(CacheFailure());
-      }
-    }
-  }
-
-  @override
-  Future<Either<Failure, NoParams>> deleteAllTodos() async{
-     if (await networkInfo.isConnected) {
-      try {
-        await remoteDataSource.deleteAllTodos();
-        return Right(NoParams());
-      } on ServerException {
-        return Left(ServerFailure());
-      }
-    } else {
-      try {
-        await localDataSource.deleteAllTodos();
-        return Right(NoParams())  ;
-      } on CacheException {
-        return Left(CacheFailure());
-      }
-    }
-  }
-
-  @override
-  Future<Either<Failure, NoParams>> deleteCompletedTodos() async{
+  Future<Either<Failure, NoParams>> _executeTodoOperation(
+    Future<void> Function() remoteOperation,
+    Future<void> Function() localOperation,
+  ) async {
     if (await networkInfo.isConnected) {
       try {
-        await remoteDataSource.deleteCompletedTodos();
+        await remoteOperation();
         return Right(NoParams());
       } on ServerException {
         return Left(ServerFailure());
       }
     } else {
       try {
-        await localDataSource.deleteCompletedTodos();
-        return Right(NoParams())  ;
+        await localOperation();
+        return Right(NoParams());
       } on CacheException {
         return Left(CacheFailure());
       }
@@ -76,27 +41,40 @@ class TodoRepositoryImpl implements TodoRepository {
   }
 
   @override
-  Future<Either<Failure, NoParams>> deleteTodoById(int id) async{
-   if (await networkInfo.isConnected) {
-      try {
-        await remoteDataSource.deleteTodoById(id);
-        return Right(NoParams());
-      } on ServerException {
-        return Left(ServerFailure());
-      }
-    } else {
-      try {
-        await localDataSource.deleteTodoById(id);
-        return Right(NoParams())  ;
-      } on CacheException {
-        return Left(CacheFailure());
-      }
-    }
+  Future<Either<Failure, NoParams>> addTodo(Todo todo) async {
+    return _executeTodoOperation(
+      () => remoteDataSource.addTodo(todo),
+      () => localDataSource.addTodo(todo),
+    );
+  }
+
+  @override
+  Future<Either<Failure, NoParams>> deleteAllTodos() async {
+    return _executeTodoOperation(
+      () => remoteDataSource.deleteAllTodos(),
+      () => localDataSource.deleteAllTodos(),
+    );
+  }
+
+  @override
+  Future<Either<Failure, NoParams>> deleteCompletedTodos() async {
+    return _executeTodoOperation(
+      () => remoteDataSource.deleteCompletedTodos(),
+      () => localDataSource.deleteCompletedTodos(),
+    );
+  }
+
+  @override
+  Future<Either<Failure, NoParams>> deleteTodoById(int id) async {
+    return _executeTodoOperation(
+      () => remoteDataSource.deleteTodoById(id),
+      () => localDataSource.deleteTodoById(id),
+    );
   }
 
   @override
   Future<Either<Failure, Todo>> getTodoById(int id) async {
-   if (await networkInfo.isConnected) {
+    if (await networkInfo.isConnected) {
       try {
         final remoteTodo = await remoteDataSource.getTodoById(id);
         return Right(remoteTodo);
@@ -115,7 +93,6 @@ class TodoRepositoryImpl implements TodoRepository {
 
   @override
   Future<Either<Failure, List<Todo>>> getTodos() async {
-
     if (await networkInfo.isConnected) {
       try {
         final remoteTodos = await remoteDataSource.getTodos();
@@ -135,62 +112,26 @@ class TodoRepositoryImpl implements TodoRepository {
   }
 
   @override
-  Future<Either<Failure, NoParams>> markTodoAsCompleted(int id)async {
-    if (await networkInfo.isConnected) {
-      try {
-        await remoteDataSource.markTodoAsCompleted(id);
-        return Right(NoParams());
-      } on ServerException {
-        return Left(ServerFailure());
-      }
-    } else {
-      try {
-        await localDataSource.markTodoAsCompleted(id);
-        return Right(NoParams())  ;
-      } on CacheException {
-        return Left(CacheFailure());
-      }
-    }
+  Future<Either<Failure, NoParams>> markTodoAsCompleted(int id) async {
+    return _executeTodoOperation(
+      () => remoteDataSource.markTodoAsCompleted(id),
+      () => localDataSource.markTodoAsCompleted(id),
+    );
   }
 
   @override
-  Future<Either<Failure, NoParams>> markTodoAsIncompleted(int id) async{
-   if (await networkInfo.isConnected) {
-      try {
-        await remoteDataSource.markTodoAsIncompleted(id);
-        return Right(NoParams());
-      } on ServerException {
-        return Left(ServerFailure());
-      }
-    } else {
-      try {
-        await localDataSource.markTodoAsIncompleted(id);
-        return Right(NoParams())  ;
-      } on CacheException {
-        return Left(CacheFailure());
-      }
-    }
+  Future<Either<Failure, NoParams>> markTodoAsIncompleted(int id) async {
+    return _executeTodoOperation(
+      () => remoteDataSource.markTodoAsIncompleted(id),
+      () => localDataSource.markTodoAsIncompleted(id),
+    );
   }
 
   @override
-  Future<Either<Failure, NoParams>> updateTodo(Todo todo) async{
-    if (await networkInfo.isConnected) {
-      try {
-        await remoteDataSource.updateTodo(todo);
-        return Right(NoParams());
-      } on ServerException {
-        return Left(ServerFailure());
-      }
-    } else {
-      try {
-        await localDataSource.updateTodo(todo);
-        return Right(NoParams())  ;
-      } on CacheException {
-        return Left(CacheFailure());
-      }
-    }
+  Future<Either<Failure, NoParams>> updateTodo(Todo todo) async {
+    return _executeTodoOperation(
+      () => remoteDataSource.updateTodo(todo),
+      () => localDataSource.updateTodo(todo),
+    );
   }
-
-
-
 }
