3bef8a729e0e4fea1bb17a99d7e58086ef2fc12a
Sanket Patel <44511437+s4nk37@users.noreply.github.com>
2024-02-29 21:28:13 +0530

    Update: added di

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,32 @@
+import 'package:flow/features/todo/presentation/bloc/todo_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 import 'core/configs/app_config.dart';
 import 'core/utils/theme/app_theme.dart';
 import 'core/utils/theme/cubit/theme_cubit.dart';
 import 'features/todo/presentation/pages/home_page.dart';
+import 'injection_container.dart' as di;
 
-Future<void> main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await AppTheme.initialize();
+  await di.init();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => ThemeCubit(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<ThemeCubit>(
+          create: (context) => di.sl<ThemeCubit>(),
+        ),
+        BlocProvider<TodoBloc>(
+          create: (context) => di.sl<TodoBloc>(),
+        ),
+      ],
       child: BlocBuilder<ThemeCubit, ThemeMode>(
         builder: (context, themeMode) {
           return MaterialApp(
